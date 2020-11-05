8a2ae74a80b68eab592c590d5a1ea97fd1721476
Pedro Brioso <pedrobrioso27@gmail.com>
2020-11-05 17:52:32 -0300

    Project Created

---
+++ projetoacad/src/main/java/com/projetoacad/projetoacad/ProjetoacadApplication.java
@@ -0,0 +1,13 @@
+package com.projetoacad.projetoacad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoacadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoacadApplication.class, args);
+	}
+
+}
---
+++ projetoacad/src/test/java/com/projetoacad/projetoacad/ProjetoacadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetoacad.projetoacad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoacadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
